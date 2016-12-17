b25ed97279ca4ac7797dd80e32bdaa722d1503c5
Jasey <nhjasey@gmail.com>
2016-12-17 16:08:52 -0500

    Create MyClone.java

---
+++ FirstWeek/MyClone/MyClone.java
@@ -0,0 +1,20 @@
+public class MyClone {
+    private String firstName;
+    private String lastName;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setFirstName(String newFirstName) {
+        firstName = newFirstName;
+    }
+    
+    public void setLastName(String newLastName) {
+        lastName = newLastName;
+    }
+}
