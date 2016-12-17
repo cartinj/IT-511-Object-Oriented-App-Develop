088afb74d4d21f26cc03c0a8e06ee18a2e2980a1
Jasey <nhjasey@gmail.com>
2016-12-17 17:47:44 -0500

    Create VirtualWorld.java

---
+++ FifthWeek/MyVirtualWorld/VirtualWorld.java
@@ -0,0 +1,10 @@
+public class VirtualWorld { // where we set our main elements for the final project
+public static void main(String[] args)
+{
+virtual.world.MyClone clone; // creates a new clone
+    clone = new virtual.world.MyClone();
+clone.setFirstName("#myname"); // sets the clone's first name
+clone.setLastName("#mylname"); // sets the clone's last name
+clone.introduction(); // invokes introduction
+}
+}
